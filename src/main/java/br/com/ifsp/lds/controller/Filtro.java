91ae0d9be62b6c41364cd31d2b1683c732c18a30
Luiz Felipe <Luiz Felipe@Helumn>
2016-09-21 14:07:45 -0300

    criação da classe de filter

---
+++ src/main/java/br/com/ifsp/lds/controller/Filtro.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifsp.lds.controller;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
+/**
+ *
+ * @author Luiz Felipe
+ */
+public class Filtro implements Filter  {
+     
+    @Override
+    public void destroy() {
+        
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(request, response);
+    }
+    
+}
