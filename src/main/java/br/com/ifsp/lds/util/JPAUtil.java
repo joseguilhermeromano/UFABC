0119034cdf306f3a4fb33d7951f0cdc8f4eb620e
Luiz Felipe <Luiz Felipe@Helumn>
2016-09-21 15:24:28 -0300

    adição do entity manager

--- src/main/java/br/com/ifsp/lds/util/JPAUtil.java
+++ src/main/java/br/com/ifsp/lds/util/JPAUtil.java
@@ -5,10 +5,19 @@
  */
 package br.com.ifsp.lds.util;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author eddie
  */
 public class JPAUtil {
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = 
+            Persistence.createEntityManagerFactory("UFABC");
     
+    public EntityManager getEntityManager() {
+        return ENTITY_MANAGER_FACTORY.createEntityManager();
+    }
 }
