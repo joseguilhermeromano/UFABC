f251ee16b7e9173ec89b2d3f1a52697722a3affd
Luiz Felipe <Luiz Felipe@Helumn>
2016-09-21 15:26:49 -0300

    adição do método para busca de usuário através do login

--- src/main/java/br/com/ifsp/lds/dao/UsuarioDAO.java
+++ src/main/java/br/com/ifsp/lds/dao/UsuarioDAO.java
@@ -5,10 +5,27 @@
  */
 package br.com.ifsp.lds.dao;
 
+import br.com.ifsp.lds.beans.Usuario;
+import br.com.ifsp.lds.util.JPAUtil;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 /**
  *
  * @author eddie
  */
 public class UsuarioDAO {
     
+    EntityManager entityManager = new JPAUtil().getEntityManager();
+    
+    public Usuario buscaUsuario(String login) {
+        try {
+            Query query = entityManager.createQuery("SELECT FROM Usuario u "
+                    + "u.login='"+login+"'");
+            Usuario usuario = (Usuario) query.getSingleResult();
+            return usuario;
+        } catch(Exception ex) {
+            return null;
+        }
+    }
 }
