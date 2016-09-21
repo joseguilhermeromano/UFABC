112a5b54f0707bfb0540c6f5614fd478e3639820
Luiz Felipe <Luiz Felipe@Helumn>
2016-09-21 14:10:03 -0300

    criação da interface Tarefa com o método index

---
+++ src/main/java/br/com/ifsp/lds/servlet/Tarefa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifsp.lds.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Luiz Felipe
+ */
+public interface Tarefa {
+    
+    public String index(HttpServletRequest req, HttpServletResponse resp);
+}
