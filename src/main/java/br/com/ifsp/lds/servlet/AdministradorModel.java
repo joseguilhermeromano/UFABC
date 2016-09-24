71bb67ec2f7d761273512151c1a68ac16a35c790
Luiz Felipe <Luiz Felipe@Helumn>
2016-09-24 16:09:34 -0300

    teste model de administrador

---
+++ src/main/java/br/com/ifsp/lds/servlet/AdministradorModel.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifsp.lds.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Luiz Felipe
+ */
+public class AdministradorModel implements Tarefa {
+
+    @Override
+    public String index(HttpServletRequest req, HttpServletResponse resp) {
+        return "index";
+    }
+    
+    public String cadastrar(HttpServletRequest req, HttpServletResponse resp) {
+        return "cadastrar";
+    }
+    
+    public String teste(HttpServletRequest req, HttpServletResponse resp) {
+        return "teste";
+    }
+}
