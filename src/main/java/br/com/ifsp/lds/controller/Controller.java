520b222601a74644dac76a98ecfb500c9da65b9e
Luiz Felipe <Luiz Felipe@Helumn>
2016-09-24 16:01:57 -0300

    lógica do front controller

--- src/main/java/br/com/ifsp/lds/controller/Controller.java
+++ src/main/java/br/com/ifsp/lds/controller/Controller.java
@@ -5,16 +5,17 @@
  */
 package br.com.ifsp.lds.controller;
 
+
 import br.com.ifsp.lds.servlet.Tarefa;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.net.URISyntaxException;
 import java.util.logging.Level;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.logging.Logger;
-import sun.util.logging.PlatformLogger;
 
 /**
  *
@@ -24,24 +25,32 @@ public class Controller extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        String classe = req.getParameter("classe");
-        String metodo = req.getParameter("metodo");
-        String pagina;
         
+        String pagina = "index";
+        String segmento = "";
         try {
-        
-            Class<?> tipo = Class.forName(classe);
+            App app = new App(req);
+            String metodo = "br.com.ifsp.lds.servlet." + app.getClasse() + "Model";
+            segmento = app.getClasse().toLowerCase();
+            
+            Class<?> tipo = Class.forName(metodo);
             Tarefa instancia = (Tarefa) tipo.newInstance();
-            Method method = tipo.getDeclaredMethod(metodo, HttpServletRequest.class, HttpServletResponse.class);
-            pagina = (String) method.invoke(instancia, req, resp);
-            req.getRequestDispatcher(pagina).forward(req, resp);
-        
-        } catch (NoClassDefFoundError | NoSuchMethodError ex) {
-            //chamar a pagina 404.
-            System.out.println(ex);
+            
+            Method method = tipo.getDeclaredMethod(app.getMetodo(),HttpServletRequest.class,HttpServletResponse.class);
+            pagina = (String) method.invoke(instancia, req,resp);
+            
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        } catch(NoClassDefFoundError | NoSuchMethodException ex) {
+            
+            //criar pagina para o erro 404
+            
         } catch (Exception ex) {
             Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        pagina = "/WEB-INF/views/"+ segmento +"/"+ pagina +".jsp";
+        
+        req.getRequestDispatcher(pagina).forward(req, resp);
     }
 }
