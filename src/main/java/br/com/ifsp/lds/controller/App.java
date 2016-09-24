3bcf0c1e1f8ec8ed3dd45602a4203db39ab588ea
Luiz Felipe <Luiz Felipe@Helumn>
2016-09-24 16:07:46 -0300

    Classe para lidar com a uri

---
+++ src/main/java/br/com/ifsp/lds/controller/App.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifsp.lds.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Luiz Felipe
+ */
+public class App {
+    
+    private String segments[];
+    private String classe;
+    private String metodo;
+    
+    public App(HttpServletRequest req) throws URISyntaxException {
+        this.segments = new URI(req.getRequestURI()).getPath().split("/");
+        this.setClasse();
+        this.setMetodo();
+    }
+    
+    private void setClasse() {
+        this.classe = this.segments[this.segments.length-2];
+        this.classe = this.formataNomeClasse();
+    }
+    
+    private String formataNomeClasse() {
+        return this.classe.substring(0,1).toUpperCase() 
+                + this.classe.substring(1);
+    }
+    
+    private void setMetodo() {
+        this.metodo = this.segments[this.segments.length-1];
+    }
+    
+    public String getMetodo() {
+        return this.metodo;
+    }
+    
+    public String getClasse() {
+        return this.classe;
+    }
+}
