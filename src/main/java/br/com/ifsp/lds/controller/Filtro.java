dc9e561db19abe855973f8416907eefc5a16f571
Luiz Felipe <Luiz Felipe@Helumn>
2016-09-26 22:09:45 -0300

    incio do tratamento de sessão

--- src/main/java/br/com/ifsp/lds/controller/Filtro.java
+++ src/main/java/br/com/ifsp/lds/controller/Filtro.java
@@ -11,6 +11,8 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 /**
@@ -31,6 +33,10 @@ public class Filtro implements Filter  {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        
+        
         chain.doFilter(request, response);
     }
     
