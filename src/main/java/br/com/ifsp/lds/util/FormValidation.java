1d923493b977ee138f374a1469322f86177538f3
rafin <rafin@rafa>
2016-10-12 15:56:23 -0300

    implantando classe de verificações

---
+++ src/main/java/br/com/ifsp/lds/util/FormValidation.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.ifsp.lds.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author rafin
+ */
+public class FormValidation {
+ 
+     private List<String> erros = new ArrayList<String>();
+    /**      
+    * @param str Como String que recebera um provável INT como conteudo
+    * @param campo Será o nome do campo no formulário
+    * @return verdadeiro ou falso para caso seja inteiro
+    */
+     public boolean isInteger(String campo, String str){
+        if(campo.isEmpty()){
+            campo =  "Campo sem nome :";
+        }
+        if (str == null || str.length() == 0) {
+            this.erros.add(campo + "Impossivel análisar um campo vazio !!! \n");
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                 this.erros.add(campo + "Apenas numeros inteiros é permitido nesse campo !!! ex:(0, 1 , 2, 3, 4, 5..) \n");
+                return false;
+            }
+        }
+        return true;
+
+    }
+    
+    /**      
+    * @param str Como String que recebera um emáil
+    * @param campo Será o nome do campo no formulário
+    * @return verdadeiro ou falso para caso seja um email válido
+    */
+     public boolean isValidEmail(String campo, String str){
+        if(campo.isEmpty()){
+            campo =  "Campo sem nome :";
+        }
+        if (str == null || str.length() == 0) {
+            this.erros.add(campo + " Campo vazio !!! \n");
+            return false;
+        }
+        
+         boolean verifica = Pattern.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}+\\.[A-Za-z]{2,}", str);
+         if(verifica){
+                return true;
+         }else{
+               this.erros.add(campo + " email inválido !!! \n");
+               return false;
+         } 
+     }
+     
+     public boolean isValidDate(){
+         
+         return false;
+         
+     }
+    
+}
