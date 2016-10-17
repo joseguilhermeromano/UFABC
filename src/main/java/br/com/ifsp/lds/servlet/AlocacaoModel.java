f89fdafaa0266c0424a0fa164f3e806bfbe8d562
luizfelipenascimento <luizfelipesilvadonascimento@gmail.com>
2016-10-17 21:41:11 -0200

    model de alocacao

---
+++ src/main/java/br/com/ifsp/lds/servlet/AlocacaoModel.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifsp.lds.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Luiz Felipe
+ */
+public class AlocacaoModel implements Tarefa {
+    private static final String[] permAdmin = {""};
+    
+    @Override
+    public String[] getPermAdmin(HttpServletRequest req, HttpServletResponse resp) {
+        return permAdmin;
+    }
+
+    @Override
+    public String cadastrar(HttpServletRequest req, HttpServletResponse resp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String alterar(HttpServletRequest req, HttpServletResponse resp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String listartudo(HttpServletRequest req, HttpServletResponse resp) {
+        
+        return "/WEB-INF/views/alocacoes.jsp";
+    }
+
+    @Override
+    public String buscar(HttpServletRequest req, HttpServletResponse resp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String excluir(HttpServletRequest req, HttpServletResponse resp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
