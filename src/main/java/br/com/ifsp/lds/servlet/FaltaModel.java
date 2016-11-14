09d76d1828742dbf96b0e1b98f4e893b678883a8
luizfelipenascimento <luizfelipesilvadonascimento@gmail.com>
2016-11-14 18:48:00 -0200

    cadastro de falta

--- src/main/java/br/com/ifsp/lds/servlet/FaltaModel.java
+++ src/main/java/br/com/ifsp/lds/servlet/FaltaModel.java
@@ -5,9 +5,16 @@
  */
 package br.com.ifsp.lds.servlet;
 
+import br.com.ifsp.lds.beans.Alocacao;
 import br.com.ifsp.lds.beans.Falta;
+import br.com.ifsp.lds.dao.AlocacaoDAO;
 import br.com.ifsp.lds.dao.FaltaDAO;
+import br.com.ifsp.lds.util.UseRules;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,11 +22,13 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author eddie
  */
-public class FaltaModel implements Tarefa{
-     private static final String[] permAdmin = {"listartudo"};
-    
+public class FaltaModel implements Tarefa {
+
+    private static final String[] permAdmin = {"listartudo"};
+
     private FaltaDAO faltaDAO = new FaltaDAO();
     private ArrayList<Falta> faltas;
+    private UseRules validation = new UseRules();
     
     @Override
     public String[] getPermAdmin(HttpServletRequest req, HttpServletResponse resp) {
@@ -28,7 +37,31 @@ public class FaltaModel implements Tarefa{
 
     @Override
     public String cadastrar(HttpServletRequest req, HttpServletResponse resp) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int codigoAlocacao = Integer.parseInt(req.getParameter("codigo"));
+        Alocacao alocacao = new AlocacaoDAO().Consultar(codigoAlocacao);
+        req.setAttribute("alocacao", alocacao);
+        
+        if (req.getParameter("cadastrar") != null) {
+            try {
+                validation.addRule("required", "data", req.getParameter("data"));
+                if(validation.executaRegras()) {
+                    SimpleDateFormat dtFormat = new SimpleDateFormat("dd-MM-yyyy");
+                    Falta falta = new Falta();
+                    falta.setData(dtFormat.parse(req.getParameter("data")));
+                    falta.setAlocacao(alocacao);
+                    faltaDAO.Cadastrar(falta);
+                } 
+                else {
+                    req.setAttribute("erro", "Não foi possível cadastrar a falta");
+                    req.setAttribute("erros", validation.getTodosErros());
+                    return "/WEB-INF/views/administrador/nova-falta.jsp";
+                } 
+            } catch (Exception e) {
+                Logger.getLogger(FaltaModel.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+
+        return "/WEB-INF/views/administrador/nova-falta.jsp";
     }
 
     @Override
@@ -38,21 +71,21 @@ public class FaltaModel implements Tarefa{
 
     @Override
     public String listartudo(HttpServletRequest req, HttpServletResponse resp) {
-       /*faltas = faltaDAO.ConsultarTudo("",req,resp);
+        /*faltas = faltaDAO.ConsultarTudo("",req,resp);
         req.setAttribute("listaFaltas", faltas);*/
         return "/WEB-INF/views/administrador/faltas.jsp";
     }
 
     @Override
     public String buscar(HttpServletRequest req, HttpServletResponse resp) {
-       int codigo = Integer.parseInt(req.getParameter("codigo"));
-       return "/WEB-INF/views/colaborador/faltas-colaborador.jsp";
-    
+        int codigo = Integer.parseInt(req.getParameter("codigo"));
+        return "/WEB-INF/views/colaborador/faltas-colaborador.jsp";
+
     }
 
     @Override
     public String excluir(HttpServletRequest req, HttpServletResponse resp) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
