bb374331ae0d91d46d101328d3c66367c1e856ce
luizfelipenascimento <luizfelipesilvadonascimento@gmail.com>
2016-11-16 14:43:47 -0200

    lista falta do somente do colaborador logado

--- src/main/java/br/com/ifsp/lds/servlet/FaltaControlador.java
+++ src/main/java/br/com/ifsp/lds/servlet/FaltaControlador.java
@@ -7,12 +7,15 @@ package br.com.ifsp.lds.servlet;
 
 import br.com.ifsp.lds.beans.Alocacao;
 import br.com.ifsp.lds.beans.Falta;
+import br.com.ifsp.lds.beans.Justificativa;
+import br.com.ifsp.lds.beans.Usuario;
 import br.com.ifsp.lds.dao.AlocacaoDAO;
 import br.com.ifsp.lds.dao.FaltaDAO;
 import br.com.ifsp.lds.util.UseRules;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
@@ -29,7 +32,7 @@ public class FaltaControlador implements Tarefa {
     private FaltaDAO faltaDAO = new FaltaDAO();
     private ArrayList<Falta> faltas;
     private UseRules validation = new UseRules();
-    
+
     @Override
     public String[] getPermAdmin(HttpServletRequest req, HttpServletResponse resp) {
         return this.permAdmin;
@@ -40,30 +43,28 @@ public class FaltaControlador implements Tarefa {
         int codigoAlocacao = Integer.parseInt(req.getParameter("codigoAlocacao"));
         Alocacao alocacao = new AlocacaoDAO().Consultar(codigoAlocacao);
         req.setAttribute("alocacao", alocacao);
-        
+
         if (req.getParameter("cadastrar") != null) {
             try {
                 validation.addRule("required", "data", req.getParameter("data"));
-                if(validation.executaRegras()) {
+                if (validation.executaRegras()) {
                     SimpleDateFormat dtFormat = new SimpleDateFormat("dd/MM/yyyy");
                     Falta falta = new Falta();
                     falta.setStatus(0);
                     falta.setData(dtFormat.parse(req.getParameter("data")));
                     falta.setAlocacao(alocacao);
-                    if(faltaDAO.Cadastrar(falta)) {
+                    if (faltaDAO.Cadastrar(falta)) {
                         req.setAttribute("sucesso", "Falta cadastrada com sucesso!");
                         return listartudo(req, resp);
-                    } 
-                    else {
+                    } else {
                         req.setAttribute("erro", "Não foi possível cadastrar a falta");
                         return "/WEB-INF/views/administrador/nova-falta.jsp";
                     }
-                } 
-                else {
+                } else {
                     req.setAttribute("erro", "Não foi possível cadastrar a falta");
                     req.setAttribute("erros", validation.getTodosErros());
                     return "/WEB-INF/views/administrador/nova-falta.jsp";
-                } 
+                }
             } catch (Exception e) {
                 Logger.getLogger(FaltaControlador.class.getName()).log(Level.SEVERE, null, e);
             }
@@ -78,6 +79,23 @@ public class FaltaControlador implements Tarefa {
 
     @Override
     public String listartudo(HttpServletRequest req, HttpServletResponse resp) {
+
+        Usuario usuario = (Usuario) req.getSession().getAttribute("usuarioLogado");
+        req.setAttribute("usuario", usuario);
+        faltas = new ArrayList<>();
+        
+        if (usuario != null) {
+            if (usuario.getAdministrador() != 1) {
+                for (Alocacao a : usuario.getAlocacoes()) {
+                    for (Falta f : a.getFaltas()) {
+                        faltas.add(f);
+                    }
+                }
+                req.setAttribute("listaFaltas", faltas);
+                return "/WEB-INF/views/colaborador/faltas-colaborador.jsp";
+            }
+        }
+
         faltas = faltaDAO.ConsultarTudo("");
         req.setAttribute("listaFaltas", faltas);
         return "/WEB-INF/views/administrador/faltas.jsp";
